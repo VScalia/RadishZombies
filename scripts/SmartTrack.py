670cd3128584994c047d997c6f55ba32ae83e9ec
GiantCowFilms <GitHub.com@GiantCowFilms.com>
2015-07-14 07:47:16 -0700

    Updated the scripts

--- scripts/SmartTrack.py
+++ scripts/SmartTrack.py
@@ -4,7 +4,7 @@ from bge import logic
 cont = logic.getCurrentController() 
 scene = logic.getCurrentScene()
 
-objectToTrack = scene.objects['bare.001']
+objectToTrack = scene.objects['player']
 
 track = cont.actuators[0]
 track.target = objectToTrack
