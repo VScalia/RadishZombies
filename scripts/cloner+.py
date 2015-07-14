7b45805266521440e2ea82acf863af5b8d9a4256
GiantCowFilms <GitHub.com@GiantCowFilms.com>
2015-07-13 20:00:52 -0700

    Clones are now working
    I changed the clones form being created with logic bricks to python.
    This avoids an error caused by the clone being in a different file to
    the player.

---
+++ scripts/cloner+.py
@@ -0,0 +1,8 @@
+import GameLogic
+from bge import logic
+
+scene = logic.getCurrentScene()        # Get the current game scene
+cont = logic.getCurrentController() # Get the controller executing this 
+owner = cont.owner                         # Get this object
+
+newZombieClone = scene.addObject('zombieClone', owner)
