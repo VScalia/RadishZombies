3cefe537e58b92af252265e674f0b745dcbdd175
GiantCowFilms <GitHub.com@GiantCowFilms.com>
2015-07-15 06:52:50 -0700

    Programmatic distribution of the pickup objects
    The pickup objects are distributed via scatter+ which also distributes
    zombieMaster

--- scripts/Scatter+.py
+++ scripts/Scatter+.py
@@ -8,15 +8,21 @@ obj = cont.owner                         # Get this object
 if not "Scattered" in obj:
     obj["Scattered"] = True
     locations = [
-    [[-76.9175,5.2857,0],[0,0,0]],
-    [[-72.948,5.6023,0],[0,0,0]],
-    [[1,1,0],[0,0,0]],
-    [[-44.7737,-75.1513,0],[0,0,0]],
-    [[58.2994,50.9079,0],[0,0,0]],
-    [[29.8511,162.6793,0],[0,0,0]],
+    [[-76.9175,5.2857,0],[0,0,0],'ZombieMaster'],
+    [[-72.948,5.6023,0],[0,0,0],'ZombieMaster'],
+    [[1,1,0],[0,0,0],'ZombieMaster'],
+    [[-44.7737,-75.1513,0],[0,0,0],'ZombieMaster'],
+    [[58.2994,50.9079,0],[0,0,0],'ZombieMaster'],
+    [[29.8511,162.6793,0],[0,0,0],'ZombieMaster'],
+    [[2,2,0],[0,0,0],'jerryCan'],
+    [[-13.6,-55,0],[0,0,0],'syrynge'],
+    [[-101.7,-34.1,0],[0,0,0],'syrynge'],
+    [[-18.6,-52.6,0],[0,0,0],'ammoCrate'],
+    [[-35,-8.6,0],[0,0,0],'ammoCrate'],
+    [[-88.3,-52.4,0],[0,0,0],'ammoCrate']
     ]
 
     for location in locations:
-        newZombie = scene.addObject('ZombieMaster', obj)
+        newZombie = scene.addObject(location[2], obj)
         newZombie.worldPosition = location[0]
         newZombie.worldOrientation = location[1]
