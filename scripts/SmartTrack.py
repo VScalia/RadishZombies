fdf9a2f6aea16492f72439df99d118025c562913
GiantCowFilms <GitHub.com@GiantCowFilms.com>
2015-07-15 08:29:53 -0700

    Zombies Track Jeep
    Added a global variable to specify the target object

--- scripts/SmartTrack.py
+++ scripts/SmartTrack.py
@@ -3,7 +3,9 @@ from bge import logic
 
 cont = logic.getCurrentController() 
 scene = logic.getCurrentScene()
+print(cont.sensors["Mount"].frameMessageCount)
 if cont.sensors["Mount"].frameMessageCount > 0:
+    print("pass")
     objectToTrack = scene.objects['bare']
 else:
     objectToTrack = scene.objects['player']
