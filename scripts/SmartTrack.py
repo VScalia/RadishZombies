3a87a05fbc5f30959401d95cf3d2b94e8c10aa1a
GiantCowFilms <GitHub.com@GiantCowFilms.com>
2015-07-15 16:24:58 -0700

    Update Smart Track

--- scripts/SmartTrack.py
+++ scripts/SmartTrack.py
@@ -3,13 +3,9 @@ from bge import logic
 
 cont = logic.getCurrentController() 
 scene = logic.getCurrentScene()
-print(cont.sensors["Mount"].frameMessageCount)
-if cont.sensors["Mount"].frameMessageCount > 0:
-    print("pass")
-    objectToTrack = scene.objects['bare']
-else:
-    objectToTrack = scene.objects['player']
-
+own = cont.owner
+print(GameLogic.track)
+objectToTrack = scene.objects[GameLogic.track]
 track = cont.actuators[0]
 track.target = objectToTrack
 cont.activate(track)
