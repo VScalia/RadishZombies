d7098ba660ce1dc2f0edd57e725787dd4a1c467d
GiantCowFilms <GitHub.com@GiantCowFilms.com>
2015-07-16 17:41:22 -0700

    Zombies are now limited in number for performance

--- scripts/cloner+.py
+++ scripts/cloner+.py
@@ -3,5 +3,6 @@ from bge import logic
 scene = logic.getCurrentScene()        # Get the current game scene
 cont = logic.getCurrentController() # Get the controller executing this 
 owner = cont.owner                         # Get this object
-
-newZombieClone = scene.addObject('zombieClone', owner)
+if owner["zombiecount"] < 90:
+	newZombieClone = scene.addObject('zombieClone', owner)
+	owner["zombiecount"] += 1
