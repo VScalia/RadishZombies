477b3a88320180e4819c40dd9f4b2615aa7edf0b
David Bates <davidjbates@live.com>
2015-07-18 11:44:08 -0400

    player mount and dismount from jeep, climb_jeep script fix and relink

--- scripts/climb_jeep.py
+++ scripts/climb_jeep.py
@@ -1,25 +1,21 @@
 import bge
 
+cont = bge.logic.getCurrentController()
+scene = bge.logic.getCurrentScene()
+
+player = scene.objects["player"]
+
 def inJeep():
-    cont = bge.logic.getCurrentController()
-    scene = bge.logic.getCurrentScene()
-
-    player = scene.objects["player"]
-
-
     player.visible = False
     #Temporary fix to return the player to previous location.
-    bge.logic.returnPos = player.worldPosition
+    bge.logic.returnPos = player.worldPosition.copy()
     player.worldPosition = [0,  0, -50]
     player.suspendDynamics()
     player.disableRigidBody()
+    print("player called from state 2 ,in jeep")
+
+
 def outJeep():
-    cont = bge.logic.getCurrentController()
-    scene = bge.logic.getCurrentScene()
-
-    player = scene.objects["player"]
-
-
     player.visible = True
     #Temporary fix to return the player to previous location.
     player.worldPosition = bge.logic.returnPos 
