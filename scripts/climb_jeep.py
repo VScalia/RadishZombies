f2b1c0530dfa23854baf549fdd5c36c048d1754e
GiantCowFilms <GitHub.com@GiantCowFilms.com>
2015-07-18 09:41:39 -0700

    Player dismount is now largely functional

--- scripts/climb_jeep.py
+++ scripts/climb_jeep.py
@@ -1,9 +1,13 @@
 import bge
+import math
 
 cont = bge.logic.getCurrentController()
 scene = bge.logic.getCurrentScene()
 
 player = scene.objects["player"]
+jeep = scene.objects["bare"]
+
+offsetFromJeep = [3.5,1]
 
 def inJeep():
     player.visible = False
@@ -18,6 +22,14 @@ def inJeep():
 def outJeep():
     player.visible = True
     #Temporary fix to return the player to previous location.
-    player.worldPosition = bge.logic.returnPos 
+    Rotation = jeep.worldOrientation.to_euler()
+    print(Rotation)
+    playerPos = [0,0,0]
+    playerPos[0] = jeep.worldPosition[0] - math.cos(Rotation.z) * offsetFromJeep[0]
+    playerPos[1] = jeep.worldPosition[1] + math.sin(Rotation.z) * offsetFromJeep[1]
+    playerPos[2] = 0
+    print(playerPos)
+    player.worldPosition = playerPos
+    player.worldOrientation =  Rotation
     player.restoreDynamics()
     player.enableRigidBody()
