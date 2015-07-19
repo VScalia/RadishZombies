646ccdddd43095bc75697fc7eb620ab8726b3ab0
GiantCowFilms <GitHub.com@GiantCowFilms.com>
2015-07-18 18:05:41 -0700

    New python cloner, WIP

--- scripts/cloner+.py
+++ scripts/cloner+.py
@@ -1,8 +1,19 @@
 from bge import logic
-
+import math
 scene = logic.getCurrentScene()        # Get the current game scene
 cont = logic.getCurrentController() # Get the controller executing this 
-owner = cont.owner                         # Get this object
-if owner["zombiecount"] < 90:
+owner = cont.owner                       # Get this object
+
+zombieRoof = 300
+
+newZombieCount = owner["zombiecount"] * 1.02
+newZombieCount = int(newZombieCount + 1)
+addZombieCount = newZombieCount - (owner["zombiecount"])
+if newZombieCount >= zombieRoof:
+	addZombieCount = addZombieCount - (newZombieCount - zombieRoof)
+
+print(newZombieCount)
+# print(addZombieCount)
+for x in range(0,addZombieCount):
 	newZombieClone = scene.addObject('zombieClone', owner)
 	owner["zombiecount"] += 1
