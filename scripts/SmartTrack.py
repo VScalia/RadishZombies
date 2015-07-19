fc12a1758bfac94d42dc13f5168d4d4b8b95a3a5
GiantCowFilms <GitHub.com@GiantCowFilms.com>
2015-07-18 19:00:35 -0700

    Die pesky debugs, die!

--- scripts/SmartTrack.py
+++ scripts/SmartTrack.py
@@ -4,7 +4,6 @@ from bge import logic
 cont = logic.getCurrentController() 
 scene = logic.getCurrentScene()
 own = cont.owner
-print(logic.track)
 objectToTrack = scene.objects[logic.track]
 track = cont.actuators[0]
 track.target = objectToTrack
