4c51f5332bfb923ec6b344e003d59250ba803cb0
David Bates <davidjbates@live.com>
2015-07-23 13:19:00 -0400

    small edit to menu_web_link.py, then relink in blend

--- scripts/menu_web_link.py
+++ scripts/menu_web_link.py
@@ -1,6 +1,6 @@
 import bge
 cont = bge.logic.getCurrentController()
 
-if cont.sensors['MouseOver'].hitObject.name == 'Sensor Repo' and cont.sensors['Mouse'].getButtonStatus(bge.events.LEFTMOUSE) == bge.logic.KX_INPUT_JUST_ACTIVATED:
+if cont.sensors['MouseOver'].hitObject == cont.owner and cont.sensors['Mouse'].getButtonStatus(bge.events.LEFTMOUSE) == bge.logic.KX_INPUT_JUST_ACTIVATED:
 	import webbrowser
 	webbrowser.open("https://github.com/VScalia/RadishZombies",0,True)
